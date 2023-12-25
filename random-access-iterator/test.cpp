cd3890742223c237880ae780001bf2f48d77bbe0
MakaRon2823 <128672716+MakaRon2823@users.noreply.github.com>
2023-12-25 10:33:16 +0300

    Update test.cpp

--- random-access-iterator/test.cpp
+++ random-access-iterator/test.cpp
@@ -1,42 +1,56 @@
 #include "tree.hpp"
-#include <algorithm>
-#include <doctest.h>
-
-TEST_CASE("can iterate") {
-    tree t{{{1, 2, 3}, 4, {5, 6, 7}}};
-    int v = 1;
-    for (int i : t) {
-        CHECK(i == v++);
-    }
-    REQUIRE(v == 8);
+tree::iterator operator+(tree::iterator const &self, ptrdiff_t diff) {
+    tree_node *node = self.p;
+    ptrdiff_t subtree_size = node->left ? node->left->subtree_size : 0;
+    if (diff >= 0 && diff < subtree_size) {
+        return operator+(tree::iterator(node->left.get()), diff);
+    } else if (diff >= subtree_size && diff < subtree_size + 1) {
+        return tree::iterator(node);
+    } else {
+        return operator+(tree::iterator(node->right.get()), diff - subtree_size - 1);
+    }
 }
 
-TEST_CASE("can reverse iterate") {
-    tree t{{{1, 2, 3}, 4, {5, 6, 7}}};
-    int v = 7;
-    std::for_each(std::make_reverse_iterator(t.end()),
-                  std::make_reverse_iterator(t.begin()),
-                  [&v](int i) { CHECK(i == v--); });
-    REQUIRE(v == 0);
+ptrdiff_t operator-(tree::iterator const &self, tree::iterator other) {
+    tree_node *node1 = self.p;
+    tree_node *node2 = other.p;
+    while (node1 != node2) {
+        if (node1->up != nullptr) {
+
+        
+        ptrdiff_t subtree_size = node1->left ? node1->left->subtree_size : 0;
+        if (node1->up->left.get() == node1) {
+
+            node1 = node1->up;
+        } else {
+
+            node1 = node1->up->left.get();
+            subtree_size += node1->subtree_size + 1;
+        }
+        }
+    }
+    return self.p->subtree_size - node1->subtree_size - 1;
+}
+auto tree::begin() const -> iterator {
+    if (root == nullptr) {
+        return tree::iterator(nullptr);
+    }
+    tree_node* node = root.get();
+    while (node->left) {
+        node = node->left.get();
+    }
+    return tree::iterator(node);
 }
 
-TEST_CASE("operator[]") {
-    tree t{{{1, 2, 3}, 4, {5, 6, 7}}};
-    for (int i = 0; i < 7; ++i)
-        CHECK(t[i] == i + 1);
-}
+auto tree::end() const -> iterator {
+    if (root == nullptr) {
+        return tree::iterator(nullptr);
+    }
+    tree_node* node = root.get();
+    while (node->right) {
+        node = node->right.get();
+    }
+    return iterator(node);
 
-TEST_CASE("operator-") {
-    tree t{{{1, 2, 3}, 4, {5, 6, {7, 8, {nil, 9, 10}}}}};
-    CHECK(t.end() - t.begin() == 10);
-    CHECK(t.begin() + (t.end() - t.begin()) == t.end());
-    for (auto i = t.begin(); i != t.end(); ++i)
-        for (auto j = t.begin(); j != t.end(); ++j)
-            CHECK(i - j == (*i) - (*j));
-}
-
-TEST_CASE("throws exception on illegal access request") {
-    tree t{{{1, 2, 3}, 4, {5, 6, {7, 8, {nil, 9, 10}}}}};
-    CHECK_THROWS_AS((void)t.at(10), std::out_of_range);
-    CHECK_THROWS_AS((void)t.at(-1), std::out_of_range);
+    
 }
